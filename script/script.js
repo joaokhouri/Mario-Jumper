f1a1ed98fad63925e132c95183fcff56f5ebeb0a
João Khouri <joaovkhouri@gmail.com>
2022-06-03 13:37:53 -0300

    v1.2 (added sounds, reset button)

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const jumpAudio = new Audio("./assets/mario-jump-sound.mp3");
+const bgMusic = new Audio("./assets/bgmusic.mp3");
+
+bgMusic.play();
+bgMusic.volume = 0.03;
+bgMusic.loop;
 
 const jump = () => {
   mario.classList.add("jump");
+  jumpAudio.play();
+  jumpAudio.volume = 0.05;
 
   setTimeout(() => {
     mario.classList.remove("jump");
@@ -20,14 +28,20 @@ const loop = setInterval(() => {
     pipe.style.left = `${pipePosition}px`;
 
     pipe.style.animation = "none";
-    pipe.style.bottom = `${marioPosition}px`;
+    pipe.style.bottom = "0";
 
     mario.src = "./assets/game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
+
+    document.getElementById("reset").style.display = "block";
 
     clearInterval(loop);
   }
 }, 10);
 
 document.addEventListener("keydown", jump);
+
+function resetGame() {
+  document.location.reload(true);
+}
